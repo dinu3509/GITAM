b3e65e125fe9e9e72d679e7660a2c14b68c9aab5
dinu3509 <skarri@19gitam.in>
2024-10-23 19:56:06 +0530

    added food-del frontend

---
+++ food-del/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>This application was developed as part of a DBMS (Database Management System) project by a group of five dedicated members: Dinesh Reddy, Aditya Vishnu, Nikam Harsh , Hardik, and Boriki Vivek, with the help of React.js for the front-end development. Leveraging React's component-based architecture, we created an intuitive and dynamic user interface that seamlessly interacts with the underlying database.</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-212-456-7890</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
